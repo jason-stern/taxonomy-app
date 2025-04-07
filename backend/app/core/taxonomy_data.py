31991fbf5f71bd485f46012597392d25f7b5b68c
Jason <jasonstern04@gmail.com>
2025-04-07 15:23:44 -0400

    Added Ardiodactyla, Perissodactyla, and Primates (unfinished)

--- backend/app/core/taxonomy_data.py
+++ backend/app/core/taxonomy_data.py
@@ -1,6 +1,13 @@
 taxonomy_data = {
     "Mammalia": {
         "Level": "Class",
+        "Artiodactyla": {
+            "Level": "Order",
+            "Bovidae": { "Level": "Family" },
+            "Cervidae": { "Level": "Family" },
+            "Giraffidae": { "Level": "Family" },
+            "Hippopotamidae": { "Level": "Family" }
+        },
         "Carnivora": {
             "Level": "Order",
             "Caniformia": {
@@ -18,6 +25,40 @@ taxonomy_data = {
             "Level": "Order",
             "Leporidae": { "Level": "Family" },
             "Ochotonidae": { "Level": "Family" },
+        },
+        "Perissodactyla": {
+            "Level": "Order",
+            "Equidae": { "Level": "Family" },
+            "Rhinocerotidae": { "Level": "Family" },
+            "Tapiridae": { "Level": "Family" }
+        },
+        "Primates": {
+            "Level": "Order",
+            "Haplorrhines": {
+                "Level": "Suborder",
+                "Simiiformes": {
+                    "Level": "Infraorder",
+                    "Catarrhines": {
+                        "Level": "Parvorder",
+                        "Cercopithecidae": { "Level": "Family" },
+                        "Homonidae": { "Level": "Family" },
+                    },
+                    "Platyrrhines": {
+                        "Level": "Parvorder",
+                        "Atelidae": { "Level": "Family" },
+                    },
+                },
+                "Tarsiiformes": {
+                    "Level": "Infraorder",
+                    "Tarsiidae": { "Level": "Family" }
+                }
+            },
+            "Strepsirrhines": {
+                "Level": "Suborder",
+                "Daubentoniidae": { "Level": "Family" },
+                "Lemuridae": { "Level": "Family" },
+                "Loridae": { "Level": "Family"}
+            }
         },
         "Rodentia": {
             "Level": "Order",
